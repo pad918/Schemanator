233eb1565a732b62b130ab68e13f2f826d39eaee
Vicaos <Vicaos@192.168.1.78>
2020-04-23 16:48:20 +0200

    Torolol.

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -14,7 +14,7 @@ public class MainClass extends javax.swing.JFrame {
      */
     public MainClass() {
         initComponents();
-    } // Faka you Månsineni
+    } // Faka you Månsineni neni
 
     /**
      * This method is called from within the constructor to initialize the form.
