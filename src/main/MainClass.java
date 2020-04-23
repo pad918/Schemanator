90fca9fe64a87a94e0e709bdd442c0bde9fc338c
Vicaos <Vicaos@192.168.1.78>
2020-04-23 16:40:55 +0200

    Jag gjorde allt

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -14,7 +14,7 @@ public class MainClass extends javax.swing.JFrame {
      */
     public MainClass() {
         initComponents();
-    }
+    } // Måns pullade någon
 
     /**
      * This method is called from within the constructor to initialize the form.
